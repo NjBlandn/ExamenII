cd3552c6d0b7ccbc148fc72d3329d7e6cba69dab
NjBlandn <navir1997@gmail.com>
2022-08-15 19:15:16 -0600

    Preprocesamiento

--- ExamenII.py
+++ ExamenII.py
@@ -34,3 +34,46 @@ name = product.getName()
 print("Name: {}".format(name))
 band_names = product.getBandNames()
 print("Band names: {}".format(", ".join(band_names)))
+
+##Crear una funcion para mostrar el producto en una
+def plotBand(product, band, vmin, vmax):
+    band = product.getBand(band)
+    w = band.getRasterWidth()
+    h = band.getRasterHeight()
+    print(w, h)
+    band_data = np.zeros(w * h, np.float32)
+    band.readPixels(0, 0, w, h, band_data)
+    band_data.shape = h, w
+    width = 12
+    height = 12
+    plt.figure(figsize=(width, height))
+    imgplot = plt.imshow(band_data, cmap=plt.cm.binary, vmin=vmin, vmax=vmax)
+    return imgplot
+
+##PRE-PROCESAMIENTO
+
+##Aplicar correccion orbital
+parameters = HashMap()
+GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+parameters.put('orbitType', 'Sentinel Precise (Auto Download)')
+parameters.put('polyDegree', '3')
+parameters.put('continueOnFail', 'false')
+apply_orbit_file = GPF.createProduct('Apply-Orbit-File', parameters, product)
+
+##Recortar la imagen
+r = shapefile.Reader("C:/Users/Dell/Desktop/Desarrollo/san manuel/shape/San_Manuel_JC.shp")
+g=[]
+for s in r.shapes():
+    g.append(pygeoif.geometry.as_shape(s))
+m = pygeoif.MultiPoint(g)
+wkt = str(m.wkt).replace("MULTIPOINT", "POLYGON(") + ")"
+#Usar el shapefile para cortar la imagen
+SubsetOp = snappy.jpy.get_type('org.esa.snap.core.gpf.common.SubsetOp')
+bounding_wkt = wkt
+geometry = WKTReader().read(bounding_wkt)
+HashMap = snappy.jpy.get_type('java.util.HashMap')
+GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+parameters = HashMap()
+parameters.put('copyMetadata', True)
+parameters.put('geoRegion', geometry)
+product_subset = snappy.GPF.createProduct('Subset', parameters, apply_orbit_file)
