6cbefb4d53790122504d84dc32211680bccc8964
NjBlandn <navir1997@gmail.com>
2022-08-15 19:25:32 -0600

    Importar comandos

---
+++ Aplicacion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 19:21:50 2022
+
+@author: Dell
+"""
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+import os
+from tkinter.messagebox import showinfo
